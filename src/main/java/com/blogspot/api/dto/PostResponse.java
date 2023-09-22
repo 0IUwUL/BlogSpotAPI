7dac5ad8a161fc3a56f6d9a098dadf677da4e627
0IUwUL <markaljonreales.com>
2023-09-22 18:11:58 +0800

    feat: added PostController Unit Test

--- src/main/java/com/blogspot/api/dto/PostResponse.java
+++ src/main/java/com/blogspot/api/dto/PostResponse.java
@@ -2,9 +2,15 @@ package com.blogspot.api.dto;
 
 import java.util.List;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 
 public class PostResponse {
     private List<PostDTO> content;
